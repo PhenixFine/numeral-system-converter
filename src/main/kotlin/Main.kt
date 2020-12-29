00abbc75fefaf170a1af77fe9b3c79a061ed390a
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-12-29 17:34:53 -0500

    Stage 3 of 6 for JetBrains Academy - Kotlin - Numerical System Converter project. • third stage has us convert a given long number to a given base using Long.toString(radix) method. It has us add prefix 0b when it's 2 - binary, 0 when 8 - octal, and 0x when 16 - hexadecimal ( I have it default to the given radix number plus B- when the radix number isn't 2, 8, or 16 ).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println((readLine()!!.toIntOrNull() ?: 0) % 8)
+    val number = readLine()!!.toLongOrNull() ?: 0L
+    val radixNum = readLine()!!.toIntOrNull() ?: 2
+    val prefix =
+        if (radixNum == 2) "0b" else if (radixNum == 8) "0" else if (radixNum == 16) "0x" else "${radixNum}B-"
+
+    println(prefix + number.toString(radixNum))
 }
