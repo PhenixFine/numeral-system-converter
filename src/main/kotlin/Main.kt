ef12bb2129353a37bd282d85e530280a2f65d166
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-12-31 18:56:32 -0500

    Stage 5 of 6 for JetBrains Academy - Kotlin - Numerical System Converter project. • fifth stage has us convert fractional numbers between bases. • main() - adjust val number to var number, and have it read as a string. If sourceRad is not equal to 10 or 1, the given number is converted to base 10 using toDecimal(). If changeRad is equal to 1, number is set to 1 repeated the value of number cast as an Int. Else if changeRad is not equal to 10, number is set to the return string of toBase(). number is then printed. • toDecimal() - takes a string number and radix Int and converts it to base 10 using the conversion formula provided in the stage. • toBase() - takes a string number ( that is of base 10 ) and radix Int, and converts it to the radix using the conversion formula provided in the stage ( and fractions are limited to 5 digits ).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,41 @@
 fun main() {
     val sourceRad = readLine()!!.toInt()
-    val number = if (sourceRad == 1) readLine()!!.length else readLine()!!.toInt(sourceRad)
+    var number = if (sourceRad == 1) readLine()!!.length.toString() else readLine()!!
     val changeRad = readLine()!!.toInt()
 
-    println(if (changeRad == 1) "1".repeat(number) else number.toString(changeRad))
+    if (sourceRad != 10 && sourceRad != 1) number = toDecimal(number, sourceRad)
+    if (changeRad == 1) number = "1".repeat(number.toInt()) else if (changeRad != 10) number =
+        toBase(number, changeRad)
+    println(number)
+}
+
+fun toDecimal(number: String, radix: Int): String {
+    return if (number.contains('.')) {
+        val decNumber = number.split('.')[0].toInt(radix).toDouble()
+        var holdDigits = 0.0
+        var times = radix.toDouble()
+
+        for (char in number.split('.')[1]) {
+            holdDigits +=
+                ((if (char.isLetter()) char.toInt() - 87 else char.toString().toInt()) / times)
+            times *= radix
+        }
+        (decNumber + holdDigits).toString()
+    } else number.toInt(radix).toString()
+}
+
+fun toBase(number: String, radix: Int): String {
+    return if (number.contains('.')) {
+        var baseNumber = number.split('.')[0].toInt().toString(radix) + "."
+        var holdDigits = ("0." + number.split('.')[1]).toDouble()
+
+        repeat(5) {
+            val holdResult = (radix * holdDigits).toString().split('.')
+
+            baseNumber += if (holdResult[0].toInt() > 9) (holdResult[0].toInt() + 87).toChar()
+                .toString() else holdResult[0]
+            if (it != 4) holdDigits = ("0." + holdResult[1]).toDouble()
+        }
+        baseNumber
+    } else number.toInt().toString(radix)
 }
