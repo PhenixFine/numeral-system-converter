6386b087cd8a92cc69389c32c512d120e2469a5b
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-01-08 19:11:51 -0500

    Stage 6 of 6 for JetBrains Academy - Kotlin - Numerical System Converter project. • I was informed that we weren't supposed to use try catch construction for exceptions, so I adjusted it accordingly. • main() - now makes use of null for catching errors, and at end either prints the string contained in var number or "error" if number is set to null. • getRange() - added for getting the number from the user and then checking that the number is within 1 to 36 for range, returning null if it isn't, and the number if it is. • radOne() - verifies that a given string is all ones, returning null if it isn't, and it converted to decimal as a string if it is. • toDecimal() - adjusted to return null if there is any problems with converting the given string and radix to decimal format. • toBase() - adjusted the formatting of line 56 and 57.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,48 @@
-import java.lang.Exception
-
 fun main() {
-    try {
-        val sourceRad = readLine()!!.toInt()
-        var number = if (sourceRad == 1) readLine()!!.length.toString() else readLine()!!
-        val changeRad = readLine()!!.toInt()
+    val sourceRad = getRange()
+    var number = if (sourceRad == null) null else readLine()
 
-        if (sourceRad != 10 && sourceRad != 1) number = toDecimal(number, sourceRad)
-        if (changeRad == 1) number = "1".repeat(number.toInt()) else if (changeRad != 10) number =
-            toBase(number, changeRad)
-        println(number)
-    } catch (e: Exception) {
-        println("error")
+    if (number != null && sourceRad != null) {
+        when {
+            sourceRad == 1 -> number = radOne(number)
+            sourceRad != 10 -> number = toDecimal(number, sourceRad)
+            number.toDoubleOrNull() == null -> number = null
+        }
     }
+    if (number != null) {
+        val changeRad = getRange()
+
+        if (changeRad != null) {
+            if (changeRad == 1) number = "1".repeat(number.toDouble().toInt())
+            else if (changeRad != 10) number = toBase(number, changeRad)
+        } else number = null
+    }
+    println(number ?: "error")
 }
 
-fun toDecimal(number: String, radix: Int): String {
+fun getRange(): Int? {
+    val holdNum = readLine() ?: return null
+    return if ((1..36).contains(holdNum.toIntOrNull())) holdNum.toInt() else null
+}
+
+fun radOne(number: String): String? {
+    for (char in number) if (char != '1') return null
+    return number.length.toString()
+}
+
+fun toDecimal(number: String, radix: Int): String? {
     return if (number.contains('.')) {
-        val decNumber = number.split('.')[0].toInt(radix).toDouble()
+        val decNumber = number.split('.')[0].toIntOrNull(radix) ?: return null
         var holdDigits = 0.0
         var times = radix.toDouble()
 
         for (char in number.split('.')[1]) {
-            holdDigits +=
-                ((if (char.isLetter()) char.toInt() - 87 else char.toString().toInt()) / times)
+            holdDigits += ((if (char.isLetter()) char.toLowerCase().toInt() - 87 else char.toString().toIntOrNull()
+                ?: return null) / times)
             times *= radix
         }
         (decNumber + holdDigits).toString()
-    } else number.toInt(radix).toString()
+    } else (number.toIntOrNull(radix) ?: return null).toString()
 }
 
 fun toBase(number: String, radix: Int): String {
@@ -38,8 +53,8 @@ fun toBase(number: String, radix: Int): String {
         repeat(5) {
             val holdResult = (radix * holdDigits).toString().split('.')
 
-            baseNumber += if (holdResult[0].toInt() > 9) (holdResult[0].toInt() + 87).toChar()
-                .toString() else holdResult[0]
+            baseNumber +=
+                if (holdResult[0].toInt() > 9) (holdResult[0].toInt() + 87).toChar().toString() else holdResult[0]
             if (it != 4) holdDigits = ("0." + holdResult[1]).toDouble()
         }
         baseNumber
